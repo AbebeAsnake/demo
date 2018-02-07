6ed85789b51d692c40857142e2947f53b707bd32
abebeasnake <abebeasnake@gmail.com>
2018-02-07 12:11:03 -0500

    new recipe pages are created

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,33 @@ public class HomeController {
     @RequestMapping("/")
     public String ShowIndex(){
         return "index";
+    }
+    @GetMapping("/gal")
+    public String gallery(){
+        return "gallery";
+    }
+    @GetMapping("/gulab")
+    public String golba(){
+        return "GolabJamun";
+    }
+    @GetMapping("/injera")
+    public String Injera(){
+        return "injera";
+    }
+    @GetMapping("/jerk")
+    public String Jerk(){
+        return "Jerk";
+    }
+    @GetMapping("/bara")
+    public String Bara(){
+        return "bara";
+    }
+    @GetMapping("/croque")
+    public String Croque(){
+        return "croque";
+    }
+    @GetMapping("/pumpkin")
+    public String Pumpkin(){
+        return "pumpkin";
     }
 }
